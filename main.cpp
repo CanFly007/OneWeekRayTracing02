d3ffc26faacf469949d326f8a05f68bc58e3c3ff
MrHe <314373866@qq.com>
2022-03-22 19:29:34 +0800

    vec3类重载运算符

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include "vec3.h"
 #include <iostream>
 
 int main() 
@@ -7,17 +8,14 @@ int main()
 
     std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
-    for (int j = image_height - 1; j >= 0; --j) 
+    for (int j = image_height - 1; j >= 0; --j) //´ÓÉÏÍùÏÂ
     {
+        std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
         for (int i = 0; i < image_width; ++i) 
         {
-            auto r = double(i) / image_width;
-            auto g = double(j) / image_height;
-            auto b = 0.2;
-            int ir = static_cast<int>(255.999 * r);
-            int ig = static_cast<int>(255.999 * g);
-            int ib = static_cast<int>(255.999 * b);
-            std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+            vec3 color(double(i) / image_width, double(j) / image_height, 0.2);
+            color.write_color(std::cout);
         }
     }
+    std::cerr << "\nDone.\n";
 }
