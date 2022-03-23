c04132fbd973c9ff8a94b555c205f5eaa9371573
MrHe <314373866@qq.com>
2022-03-23 11:31:22 +0800

    新增ray.h，按摄像机到该像素的normalize向量的Y值输出颜色，像素高度在[-1,1]之间

--- main.cpp
+++ main.cpp
@@ -1,5 +1,13 @@
 #include "vec3.h"
+#include "ray.h"
 #include <iostream>
+
+vec3 ray_color(const ray& r)
+{
+    vec3 unit_direction = unit_vector(r.direction());
+    double t = 0.5 * (unit_direction.y() + 1.0);
+    return (1.0 - t) * vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
+}
 
 int main() 
 {
@@ -7,13 +15,19 @@ int main()
     const int image_height = 100;
 
     std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
-
+    vec3 lower_left_corner(-2.0, -1.0, -1.0);
+    vec3 horizontal(4.0, 0.0, 0.0);
+    vec3 vertical(0.0, 2.0, 0.0);
+    vec3 origin(0.0, 0.0, 0.0);
     for (int j = image_height - 1; j >= 0; --j) //从上往下
     {
         std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
         for (int i = 0; i < image_width; ++i) 
         {
-            vec3 color(double(i) / image_width, double(j) / image_height, 0.2);
+            auto u = double(i) / image_width;
+            auto v = double(j) / image_height;//从上到下1到0
+            ray r(origin, lower_left_corner + u * horizontal + v * vertical);
+            vec3 color = ray_color(r);
             color.write_color(std::cout);
         }
     }
