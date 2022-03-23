e5e3141426bf8a1234f55b46542280c9377b75fd
MrHe <314373866@qq.com>
2022-03-23 20:24:03 +0800

    返回直线系数t，相交点和球心相减就是法线

--- vec3.h
+++ vec3.h
@@ -81,6 +81,11 @@ inline vec3 operator/(vec3 v, double t)
 {
 	return (1 / t) * v;
 }
+inline vec3 operator+(vec3 v, double i)
+{
+	return vec3(v.e[0] + i, v.e[1] + i, v.e[2] + i);
+}
+
 inline double dot(const vec3& u, const vec3& v)
 {
 	return u.e[0] * v.e[0] + u.e[1] * v.e[1] + u.e[2] * v.e[2];
