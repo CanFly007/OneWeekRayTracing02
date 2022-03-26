0d3a19a884b1860942e4dca6a9278d8fd4b658fe
MrHe <314373866@qq.com>
2022-03-26 18:02:59 +0800

    在一个像素内发射100条射线，取他们碰到的颜色，然后求平均，以达到抗锯齿效果，边缘时候ray在一个像素内可能碰到背景可能碰到球，进行混合

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 
 #include "hittable_list.h"
 #include "sphere.h"
+#include "camera.h"
 #include <iostream>
 
 double hit_sphere(const vec3& center, double radius, const ray& r)//返回直线系数t（最小那个最近），或者-1没碰到
@@ -34,6 +35,7 @@ int main()
 {
     const int image_width = 200;
     const int image_height = 100;
+    const int samples_per_pixel = 100;
 
     std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
     vec3 lower_left_corner(-2.0, -1.0, -1.0);//下面四项是世界坐标
@@ -44,16 +46,26 @@ int main()
     hittable_list world;
     world.add(make_shared<sphere>(vec3(0, 0, -1), 0.5));
     world.add(make_shared<sphere>(vec3(0, -100.5, -1), 100));
+    camera cam;
 
     for (int j = image_height - 1; j >= 0; --j) //从上往下
     {
         std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
         for (int i = 0; i < image_width; ++i) 
         {
-            auto u = double(i) / image_width;
-            auto v = double(j) / image_height;//从上到下1到0
-            ray r(origin, lower_left_corner + u * horizontal + v * vertical);//u和v的范围是[0,1]，r是在世界坐标
-            vec3 color = ray_color(r, world);
+            vec3 color(0, 0, 0);
+            for (int s = 0; s < samples_per_pixel; ++s)
+            {
+                auto u = (i + random_double()) / image_width;//random_double()在一个像素内的随机
+                auto v = (j + random_double()) / image_height;//v的值从上到下1到0
+                ray r = cam.get_ray(u, v);
+                color += ray_color(r, world);//此时color是爆表颜色
+            }
+            color /= samples_per_pixel;
+            auto r = clamp(color.e[0], 0.0, 0.99);
+            auto g = clamp(color.e[1], 0.0, 0.99);
+            auto b = clamp(color.e[2], 0.0, 0.99);
+            color = vec3(r, g, b);
             color.write_color(std::cout);
         }
     }
