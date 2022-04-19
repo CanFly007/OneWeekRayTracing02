8bda14dbd4a7f5cf76ddf519aed734c8aea78ec8
MrHe <314373866@qq.com>
2022-04-19 10:58:28 +0800

    一维：某两块区域是否有重叠算法，有重叠则更新到重叠区域，再和下一维度区域比较

--- aabb.h
+++ aabb.h
@@ -19,14 +19,24 @@ public:
 	//判断ray和这个aabb盒子是否相交
 	bool hit(const ray& r, double tmin, double tmax)const
 	{
-		//xyz三个轴，每个轴算出来的tmin和tmax都表示直线上固定一个点位置
+		//xyz三个轴，每个轴算出来的tmin和tmax都表示直线上固定一个点位置t，用t做是否重合算法
 		for (int axis = 0; axis < 3; axis++)
 		{
 			double t0 = (_min[axis] - r.origin()[axis]) / r.direction()[axis];//比如X轴上，aabb盒子和射线交点：射线的t0位置
 			double t1 = (_max[axis] - r.origin()[axis]) / r.direction()[axis];
 			//保持t0一直小于t1
-			double smaller = ffmin(t0, t1);
+			double smallerT = ffmin(t0, t1);
+			double biggerT = ffmax(t0, t1);//smallT和biggerT是射线与aabb盒子X轴的交点 t位置
+
+			//重叠区域算法，可以画重叠，一条在另外一条全左，在另外一条全右，三条线段直观看
+			//射线tmin到tmax区段 是否和交点区段有重合区域
+			tmin = ffmax(smallerT, tmin);
+			tmax = ffmin(biggerT, tmax);
+			if (tmax <= tmin)//该线段区间在X维度都没有重叠区域了，就不用看Y和Z维度了
+				return false;
+			//不返回false，则说明有重叠，ray的tmin和tmax更新为与X维度的重叠部分，再接下来判断更新后的部分在Y和Z维度情况
 		}
+		return true;
 	}
 };
 
@@ -43,5 +53,4 @@ aabb surrounding_box(aabb box0, aabb box1)
 
 	return aabb(small, big);
 }
-
 #elif
