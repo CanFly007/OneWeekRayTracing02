5a9f5a6f723c1bb2446159ba9993b76d36bc992b
MrHe <314373866@qq.com>
2022-04-22 11:46:45 +0800

    get_sphere_uv中，p的值需要在半径为1的球上，球面坐标系转笛卡尔的cos/sin计算中的被除数才是半径1，球的uv算出来才是球面坐标系下正确结果

--- sphere.h
+++ sphere.h
@@ -22,11 +22,19 @@ private:
     //球的uv，按照球面坐标系 u为水平phi角度，v是竖直方向theta角度，然后将角度范围归一化到[0,1]
     static void get_sphere_uv(const vec3& p, double& u, double& v)
     {
+        // p: a given point on the sphere of radius one, centered at the origin.
+        // p的值需要在单位球上，即半径为1时候，球面坐标转笛卡尔坐标sin，cos这些函数算的除法才能省略
+        // u: returned value [0,1] of angle around the Y axis from X=-1.
+        // v: returned value [0,1] of angle from Y=-1 to Y=+1.
+        //     <1 0 0> yields <0.50 0.50>       <-1  0  0> yields <0.00 0.50>
+        //     <0 1 0> yields <0.50 1.00>       < 0 -1  0> yields <0.50 0.00>
+        //     <0 0 1> yields <0.25 0.50>       < 0  0 -1> yields <0.75 0.50>
+
         double phi = atan2(p.z(), p.x());//[-pi, pi] phi是与x正方向的夹角，且朝向z轴正方向为正值，只有这样笛卡尔坐标系转换时正负才是对的
         double theta = asin(p.y());//按这个公式，是在单位圆上，theta范围是[-pi/2, pi/2]
         
         u = (phi + pi) / (2 * pi);//右手坐标系，z轴正方向是pi/2，对应u是+0.75，所以从上往下看是从x负轴开始顺时针一圈
-        //u = 1 - u;//从上往下看，从x负轴开始逆时针一圈
+        u = 1 - u;//从上往下看，从x负轴开始逆时针一圈
         v = (theta + pi / 2) / pi;//球顶theta是pi/2，对应v是1，球底v对应0。从1到0
     }
 };
@@ -50,7 +58,9 @@ bool sphere::hit(const ray& r, double tmin, double tmax, hit_record& rec) const/
             rec.normal = dot(r.direction(), rec.normal) < 0 ? rec.normal : (-rec.normal);//射线从内部射入，法线也在内部
 			rec.mat_ptr = mat_ptr;//碰撞的点记录的材质 为当前类对象的材质
             //hit到这个sphere后，还得根据hit的point算出hit到哪个uv值上
-            get_sphere_uv(rec.p, rec.u, rec.v);//rec.u和rec.v是函数的out输出值
+            //如果p不是单位圆上的点，需要一一归一化成单位圆上对应的点，去算uv值
+            vec3 p = (rec.p - center) / radius;
+            get_sphere_uv(p, rec.u, rec.v);//rec.u和rec.v是函数的out输出值
             return true;
         }
         t = (-b + sqrt(discriminant)) / (2.0 * a);//因为近的点不在规定区间，用远的点尝试
@@ -61,7 +71,9 @@ bool sphere::hit(const ray& r, double tmin, double tmax, hit_record& rec) const/
             rec.normal = (rec.p - center) / radius;
             rec.normal = dot(r.direction(), rec.normal) < 0 ? rec.normal : (-rec.normal);//射线从内部射入，法线也在内部
 			rec.mat_ptr = mat_ptr;//碰撞的点记录的材质 为当前类对象的材质
-            get_sphere_uv(rec.p, rec.u, rec.v);
+            //如果p不是单位圆上的点，需要一一归一化成单位圆上对应的点，去算uv值
+            vec3 p = (rec.p - center) / radius;
+            get_sphere_uv(p, rec.u, rec.v);
             return true;
         }
     }
